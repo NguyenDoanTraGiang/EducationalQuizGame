993ceffd38aca5fd055b65aec9f35827537fc81c
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2022-01-20 09:47:27 +0800

    Get data from intent fromm GameActivity

--- app/src/main/java/au/edu/jcu/cp3406/educationalquizgame/HighScoreActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/educationalquizgame/HighScoreActivity.java
@@ -2,13 +2,27 @@ package au.edu.jcu.cp3406.educationalquizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HighScoreActivity extends AppCompatActivity {
+  Integer correctAnswerNum;
+  Integer maxQuestionNum;
+  TextView exampleDisplay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+
+        Intent intent = getIntent();
+        correctAnswerNum = intent.getIntExtra("correctAnswerNum", 0);
+        maxQuestionNum = intent.getIntExtra("maxQuestionNum", 0);
+
+        exampleDisplay = (TextView) findViewById(R.id.recentScoreDisplay);
+        exampleDisplay.setText(String.format("Recent attempt: %1$s out of %2$s", correctAnswerNum, maxQuestionNum));
+
     }
 }
