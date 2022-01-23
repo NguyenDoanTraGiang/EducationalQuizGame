b9cc8639fa5343bfe62d726d3c5a7d8e6276ac88
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2022-01-23 16:59:19 +0800

    Remove warnings

--- app/src/main/java/au/edu/jcu/cp3406/educationalquizgame/SettingActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/educationalquizgame/SettingActivity.java
@@ -15,42 +15,42 @@ public class SettingActivity extends AppCompatActivity {
   Spinner maxQuesSpinner;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_setting);
 
-        // create spinners
-      timeLimitSpinner = (Spinner) findViewById(R.id.timeSpinner);
-      maxQuesSpinner = (Spinner) findViewById(R.id.maxQuesSpinner);
+    // create spinners
+    timeLimitSpinner = findViewById(R.id.timeSpinner);
+    maxQuesSpinner = findViewById(R.id.maxQuesSpinner);
 
-      // create adapters from suitable arrays in strings.xml
-      ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter.createFromResource(this, R.array.timeLimits, R.layout.spinner_style);
-      ArrayAdapter<CharSequence> maxQuesAdapter = ArrayAdapter.createFromResource(this, R.array.maxQuesNum, R.layout.spinner_style);
+    // create adapters from suitable arrays in strings.xml
+    ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter.createFromResource(this, R.array.timeLimits, R.layout.spinner_style);
+    ArrayAdapter<CharSequence> maxQuesAdapter = ArrayAdapter.createFromResource(this, R.array.maxQuesNum, R.layout.spinner_style);
 
-      // apply Adapter array to respective Spinner
-      timeLimitSpinner.setAdapter(timeAdapter);
-      maxQuesSpinner.setAdapter(maxQuesAdapter);
+    // apply Adapter array to respective Spinner
+    timeLimitSpinner.setAdapter(timeAdapter);
+    maxQuesSpinner.setAdapter(maxQuesAdapter);
 
-      // set default values
-      timeLimitSpinner.setSelection(2);
-      maxQuesSpinner.setSelection(3);
-    }
+    // set default values
+    timeLimitSpinner.setSelection(2);
+    maxQuesSpinner.setSelection(3);
+  }
 
   public void applyBtnClicked(View view) {
-      // get selected item from Spinner
-      String timeLimitText = timeLimitSpinner.getSelectedItem().toString();
-      String maxQuesText = maxQuesSpinner.getSelectedItem().toString();
-      // convert to Integer
-      int timeLimit = Integer.parseInt(timeLimitText);
-      int maxQues = Integer.parseInt(maxQuesText);
+    // get selected item from Spinner
+    String timeLimitText = timeLimitSpinner.getSelectedItem().toString();
+    String maxQuesText = maxQuesSpinner.getSelectedItem().toString();
+    // convert to Integer
+    int timeLimit = Integer.parseInt(timeLimitText);
+    int maxQues = Integer.parseInt(maxQuesText);
 
-      // add data to intent so Main Activity can access them
-      Intent intent = new Intent();
-      intent.putExtra("timeLimit", timeLimit);
-      intent.putExtra("maxQuestion", maxQues);
-      setResult(RESULT_OK, intent);
-      finish();
+    // add data to intent so Main Activity can access them
+    Intent intent = new Intent();
+    intent.putExtra("timeLimit", timeLimit);
+    intent.putExtra("maxQuestion", maxQues);
+    setResult(RESULT_OK, intent);
+    finish();
   }
 
   // return to Main Activity without putting data into intent
